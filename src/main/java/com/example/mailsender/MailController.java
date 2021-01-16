4f1e21d798f80f715f2584c02010fb8527af73eb
SubAkBa <circle5926@gmail.com>
2021-01-17 01:09:44 +0900

    add Authentication email

--- src/main/java/com/example/mailsender/MailController.java
+++ src/main/java/com/example/mailsender/MailController.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+
 @Controller
 @RequiredArgsConstructor
 public class MailController {
@@ -18,5 +20,16 @@ public class MailController {
     @PostMapping("/mail")
     public void execMail(Mail mail) {
         mailService.sendMail(mail);
+    }
+
+//    @PostMapping("/mail")
+//    public void authMail(Mail mail) {
+//        mailService.sendAuthMail(mail);
+//    }
+
+    @PostMapping("/mail-auth")
+    @ResponseBody
+    public boolean check(@RequestBody Map<String, String> answer){
+        return mailService.check(answer.get("answer"));
     }
 }
--- src/main/java/com/example/mailsender/MailService.java
+++ src/main/java/com/example/mailsender/MailService.java
@@ -5,12 +5,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
+import java.util.Random;
+
 @Service
 @AllArgsConstructor
 public class MailService {
 
     private JavaMailSender sender;
-    private static final String from_address = "sonwonjoo@naver.com";
+    private static StringBuilder key;
 
     public void sendMail(Mail mail) {
         try {
@@ -19,7 +21,7 @@ public class MailService {
             handler.setTo(mail.getAddress());
             handler.setSubject(mail.getTitle());
 
-            String htmlContent = "<p>" + mail.getMessage() + "</p>";
+            String htmlContent = "<p>" + mail.getMessage() + "</p> <img src='cid:sample-img'>";
             handler.setText(htmlContent, true);
 
             handler.setAttach(mail.getFile().getOriginalFilename(), mail.getFile());
@@ -29,5 +31,39 @@ public class MailService {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public void sendAuthMail(Mail mail) {
+        try {
+            MailHandler mailHandler = new MailHandler(sender);
+            mailHandler.setTo(mail.getAddress());
+            mailHandler.setSubject("인증메일입니다.");
+            Random rnd = new Random();
+            key = new StringBuilder();
+
+            for (int i = 0; i < 6; i++) {
+                int index = rnd.nextInt(3);
+                switch (index) {
+                    case 0:
+                        key.append(((int) (rnd.nextInt(26)) + 97));
+                        break;
+                    case 1:
+                        key.append(((int) (rnd.nextInt(26)) + 65));
+                        break;
+                    case 2:
+                        key.append((rnd.nextInt(10)));
+                        break;
+                }
+            }
+            String htmlContent = "<p> 인증번호:" + key.toString() + "</p>";
+            mailHandler.setText(htmlContent, true);
+            mailHandler.send();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean check(String input) {
+        return key.toString().equals(input);
     }
 }
